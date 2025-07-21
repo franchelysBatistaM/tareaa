35b2a3d7f5980f359859adb6f820fd03264d43ad
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-07-21 14:44:32 -0400

    Creando UsuarioDeportivo/entities/domain

---
+++ new/tar34/ActividadDeportiva.Domain/Entities/UsuarioDeportivo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActividadDeportiva.Domain.Core;
+
+namespace ActividadDeportiva.Domain.Entities
+{
+    public class UsuarioDeportivo : BaseEntity
+    {
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? Correo { get; set; }
+        public string? Telefono { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public double PesoKg { get; set; }
+        public double AlturaCm { get; set; }
+        public string? Genero { get; set; }
+    }
+}
