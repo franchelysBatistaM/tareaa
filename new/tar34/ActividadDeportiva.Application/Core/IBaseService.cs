5fdb46eaa5d8232ab179468be6bd2e255737c84b
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-07-21 14:36:03 -0400

    Creando IBaseService

---
+++ new/tar34/ActividadDeportiva.Application/Core/IBaseService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadDeportiva.Application.Core
+{
+    public interface IBaseService<TDto>
+    {
+        Task<List<TDto>> ObtenerTodosAsync();
+        Task<TDto?> ObtenerPorIdAsync(int id);
+        Task AgregarAsync(TDto dto);
+        Task ActualizarAsync(TDto dto);
+        Task EliminarAsync(int id);
+    }
+
+}
