d4960b2cfc68982dbbc0fdb4c63f59e40cb8ff9e
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-07-21 15:51:59 -0400

    creando IUsuarioDeportivoService.cs

---
+++ new/tar34/ActividadDeportiva.Application/Contract/IUsuarioDeportivoService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ActividadDeportiva.Application.Dtos.UsuarioDeportivo;
+
+namespace ActividadDeportiva.Application.Contract
+{
+    public interface IUsuarioDeportivoService
+    {
+        Task<List<UsuarioDeportivoDto>> ObtenerTodos();
+        Task<UsuarioDeportivoDto?> ObtenerPorId(int id);
+        Task Crear(UsuarioDeportivoDto usuarioDto);
+        Task Actualizar(UsuarioDeportivoDto usuarioDto);
+        Task Eliminar(int id);
+    }
+}
