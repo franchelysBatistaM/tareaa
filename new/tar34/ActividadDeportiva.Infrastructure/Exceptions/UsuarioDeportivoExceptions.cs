1430e5ebf7ee8cb4d99e21d050dd8044682b460f
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-07-21 14:18:38 -0400

    Creando usuariodeportivoExceptions

---
+++ new/tar34/ActividadDeportiva.Infrastructure/Exceptions/UsuarioDeportivoExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadDeportiva.Infrastructure.Exceptions
+{
+    public class UsuarioDeportivoNotFoundException : Exception
+    {
+        public UsuarioDeportivoNotFoundException(int id)
+            : base($"El usuario deportivo con ID {id} no fue encontrado.")
+        {
+        }
+    }
+}
