381f874f2714f47664601ddcfd6286cd284f0731
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-07-21 14:38:29 -0400

    Implementando UsuarioDeportivoRepository

---
+++ new/tar34/ActividadDeportiva.Infrastructure/Repositories/UsuarioDeportivoRepository.cs
@@ -0,0 +1,16 @@
+﻿using ActividadDeportiva.Domain.Entities;
+using ActividadDeportiva.Infrastructure.Context;
+using ActividadDeportiva.Infrastructure.Core;
+using ActividadDeportiva.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ActividadDeportiva.Infrastructure.Repositories
+{
+    public class UsuarioDeportivoRepository : BaseRepository<UsuarioDeportivo>, IUsuarioDeportivoRepository
+    {
+        public UsuarioDeportivoRepository(ActividadDeportivaContext context)
+            : base(context)
+        {
+        }
+    }
+}
