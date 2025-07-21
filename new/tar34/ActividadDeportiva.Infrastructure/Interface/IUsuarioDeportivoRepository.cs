b9957146ebbc674a2551294660d5f3c71dc6af77
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-07-21 15:50:38 -0400

    Creando IUsuarioDeportivoRepository.cs

---
+++ new/tar34/ActividadDeportiva.Infrastructure/Interface/IUsuarioDeportivoRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActividadDeportiva.Domain.Entities;
+
+namespace ActividadDeportiva.Infrastructure.Interfaces
+{
+    public interface IUsuarioDeportivoRepository
+    {
+        Task<List<UsuarioDeportivo>> ObtenerTodosAsync();
+        Task<UsuarioDeportivo?> ObtenerPorIdAsync(int id);
+        Task AgregarAsync(UsuarioDeportivo usuario);
+        Task ActualizarAsync(UsuarioDeportivo usuario);
+        Task EliminarAsync(UsuarioDeportivo usuario);
+    }
+}
